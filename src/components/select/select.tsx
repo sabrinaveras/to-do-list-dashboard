7ff9acc8b81374785567b545280cb19d6c6c3205
Sabrina Veras <sabrina.veras@perfectflightapp.com>
2025-04-16 18:08:37 -0300

    refactoring select component

--- src/components/select/select.tsx
+++ src/components/select/select.tsx
@@ -1,68 +1,77 @@
-import { useState } from "react"
+import { Arrow } from "@/design/icons";
+import { useState } from "react";
 
-interface SelectProps{
-    label: string;
-    options: Option[];
-    onChange: (option: Option) => void;
+interface SelectProps {
+  label: string;
+  options: Option[];
+  onChange: (option: Option) => void;
 }
 
-interface Option{
-    id: string;
-    label: string;
-    selected?: boolean;
+interface Option {
+  id: string;
+  label: string;
+  selected?: boolean;
 }
 
 export const Select = ({ label, options, onChange }: SelectProps) => {
-    const [isOpen, setIsOpen] = useState(false)
-    // const [selectedOption, setSelectedOption] = useState<Option[]>(options.filter(option => option.selected))
+  const [isOpen, setIsOpen] = useState(false);
+  // const [selectedOption, setSelectedOption] = useState<Option[]>(options.filter(option => option.selected))
 
-    const toggleDropdown = () => {
-        setIsOpen(!isOpen)
-    }
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleOptionClick = (option: Option) => {
-        onChange(option);
-        // setSelectedOption([option])
-        setIsOpen(false)
-    }
+  const handleOptionClick = (option: Option) => {
+    onChange(option);
+    // setSelectedOption([option])
+    setIsOpen(false);
+  };
 
-    return (
-        <div className="relative w-[64px] h-[32px]">
-            <button type="button" onClick={toggleDropdown}
-            className="flex items-center justify-between w-full px-[4px] py-[2px] bg-nav-bar-dark"
-            >
-                <span>{label}</span>
-            </button>
-
-            {/* dropdown */}
-            {
-                isOpen && (
-                    <div className="absolute z-10 w-full mt-[1px] rounded-md shadow-lg bg-white">
-                        <div className="relative py-[2px]">
-                            {/* vertical line */}
-                            <div className="absolute left-[6px] top-[0px] bottom-[0px] w-px"></div>
-
-                            {/* options */}
-                            <ul>
-                                {
-                                    options.map((option) => {
-                                        return (
-                                            <li key={option.id} onClick={() => handleOptionClick(option)}
-                                            className="relative flex items-center px-[4px] py-[2px] cursor-pointer"
-                                            >
-                                                <div className="absolute left-[6px] top-1/2 w-[4px] h-px"></div> 
-                                                <span className="ml-[10px]">{option.label}</span>
-
-                                            </li>
-                                        )
-                                    })
-                                }
-                            </ul>
-                        </div>
-
-                    </div>
-                )
-            }
+  return (
+    <div className="relative w-full">
+      <button
+        type="button"
+        onClick={toggleDropdown}
+        className="flex items-center justify-between w-full px-[4px] py-[2px] mb-[8px] font-medium text-base"
+      >
+        <div className="flex flex-col items-start">
+          <span className="font-medium">{label}</span>
         </div>
-    )
-}
+        <div
+          className={`transform transition-transform duration-200 ${isOpen ? "rotate-180" : ""}`}
+        >
+          <Arrow />
+        </div>
+      </button>
+
+      {/* dropdown */}
+      <div
+        className={`absolute z-[10px] w-full mt-[1px] bg-white transform transition-all duration-200 ease-in-out ${isOpen ? "opacity-100 scale-y-100" : "opacity-0 scale-y-95 pointer-events-none"}`}
+      >
+        <div className="relative py-[12px]">
+          {/* vertical line */}
+          <div className="absolute left-[6px] top-[0px] bottom-[0px] w-[2px] bg-grey"></div>
+
+          {/* options */}
+          <ul className="space-y-1">
+            {options.map((option) => (
+              <li
+                key={option.id}
+                onClick={() => handleOptionClick(option)}
+                className="relative flex items-center cursor-pointer group"
+              >
+                <div className="absolute left-[6px] top-1/2 w-[14px] h-[2px] bg-grey"></div>
+                <span
+                  className={`ml-[34px] px-[12px] py-[12px] text-sm transition-colors
+                    ${option.selected ? "bg-grey rounded-full" : "text-gray-600 hover:text-gray-900"}`}
+                >
+                  {option.label}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/sideBar/sideBar.tsx
+++ src/components/sideBar/sideBar.tsx
@@ -1,14 +1,20 @@
-import { Select } from "../select"
+import { Select } from "../select";
 
 export const SideBar = () => {
-    const projectOptions = [
-        { id: "all", label: "All projects (3)" },
-        { id: "design", label: "Design system", selected: true },
-        { id: "flow", label: "User flow" },
-        { id: "research", label: "UX research" },
-      ]
+  const projectOptions = [
+    { id: "all", label: "All projects (3)" },
+    { id: "design", label: "Design system", selected: true },
+    { id: "flow", label: "User flow" },
+    { id: "research", label: "UX research" },
+  ];
 
-    return <div className="bg-side-panel">
-        <Select label="Projects" options={projectOptions} onChange={(option) => console.log("Selected:", option)} />
+  return (
+    <div className="bg-side-panel px-[28px]">
+      <Select
+        label="Projects"
+        options={projectOptions}
+        onChange={(option) => console.log("Selected:", option)}
+      />
     </div>
-}
+  );
+};
