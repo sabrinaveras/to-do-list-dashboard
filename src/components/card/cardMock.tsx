472c05e9195a07cbdfdd4f4e6722788d9a268557
Sabrina Veras <sabrina.veras@perfectflightapp.com>
2025-04-18 18:02:18 -0300

    add cardMock component

---
+++ src/components/card/cardMock.tsx
@@ -0,0 +1,7 @@
+export const CardMock = () => {
+  return (
+    <div className="flexw-[320px] h-[178px] flex border-dashed border-[3px] rounded-[12px] color-custom-grey items-center justify-center my-[14px]">
+      <span className="color-custom-dark-50">Drag your task here</span>
+    </div>
+  );
+};
