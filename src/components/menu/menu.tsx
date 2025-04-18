78f33fecb4a1170af4cc3c3dc451c469364118f4
Sabrina Veras <sabrina.veras@perfectflightapp.com>
2025-04-18 15:04:36 -0300

    add new component menu

---
+++ src/components/menu/menu.tsx
@@ -0,0 +1,9 @@
+export const Menu = () => {
+  return (
+    <div className="flex items-center gap-[3px] border-[4px] border-solid border-grey rounded-full px-[6px] py-[12px]">
+      <div className="w-[3px] h-[3px] bg-dark rounded-full"></div>
+      <div className="w-[3px] h-[3px] bg-dark rounded-full"></div>
+      <div className="w-[3px] h-[3px] bg-dark rounded-full"></div>
+    </div>
+  );
+};
